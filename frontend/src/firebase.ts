ee67a807bdc7012e9a3206824edab4fa11cda7d2
Sarathy <144701759+Sarathy2908@users.noreply.github.com>
2026-03-12 13:21:49 +0530

    Update firebase.ts

--- frontend/src/firebase.ts
+++ frontend/src/firebase.ts
@@ -2,12 +2,12 @@
 import { initializeApp } from 'firebase/app'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDZN7DH1GbfU__EoBOX-xSXu0ConD9BWE0",
-  authDomain: "intelli-85a56.firebaseapp.com",
-  projectId: "intelli-85a56",
-  storageBucket: "intelli-85a56.firebasestorage.app",
-  messagingSenderId: "6176307628",
-  appId: "1:6176307628:web:321b079d81227cd502e209"
+  apiKey: apiKey,
+  authDomain: authDomain,
+  projectId: projectId,
+  storageBucket: storageBucket,
+  messagingSenderId:messagingSenderId,
+  appId:  appId
 };
 
 export const firebaseApp = initializeApp(firebaseConfig)
